b5519433682e4449c96c89de062ac8c2e991551d
olengs <limjunchen37@gmail.com>
2026-03-17 14:13:28 +0800

    Polymarket data

--- app.py
+++ app.py
@@ -2,38 +2,45 @@ from roostoo import Roostoo
 import os
 import dotenv
 dotenv.load_dotenv()
+from polymarket import PolyMarket
 
 MODE = os.getenv("MODE")
 API_KEY = os.getenv(f"{MODE}_API_KEY")   # Replace with your actual API key
 SECRET_KEY = os.getenv(f"{MODE}_API_SECRET") # Replace with your actual secret key
+slug = "what-price-will-bitcoin-hit-march-16-22"
 
 if __name__ == "__main__":
     
     broker = Roostoo(API_KEY, SECRET_KEY)
-    print(f"API_KEY: {API_KEY}")
-    print(f"SECRET_KEY: {SECRET_KEY}")
-    print("\n--- Checking Server Time ---")
-    print(broker.check_server_time())
+    market = PolyMarket()
+    x = market.get_market_data("Bitcoin price on March 17?")
+    PolyMarket.print_market_data(x)
+    #print(x)
+    
+    # print(f"API_KEY: {API_KEY}")
+    # print(f"SECRET_KEY: {SECRET_KEY}")
+    # print("\n--- Checking Server Time ---")
+    # print(broker.check_server_time())
 
-    print("\n--- Getting Exchange Info ---")
-    info = broker.get_exchange_info()
-    if info:
-        print(f"Available Pairs: {list(info.get('TradePairs', {}).keys())}")
+    # print("\n--- Getting Exchange Info ---")
+    # info = broker.get_exchange_info()
+    # if info:
+    #     print(f"Available Pairs: {list(info.get('TradePairs', {}).keys())}")
 
-    print("\n--- Getting Market Ticker (BTC/USD) ---")
-    ticker = broker.get_ticker("BTC/USD")
-    if ticker:
-        print(ticker.get("Data", {}).get("BTC/USD", {}))
+    # print("\n--- Getting Market Ticker (BTC/USD) ---")
+    # ticker = broker.get_ticker("BTC/USD")
+    # if ticker:
+    #     print(ticker.get("Data", {}).get("BTC/USD", {}))
 
-    print("\n--- Getting Account Balance ---")
-    print(broker.get_balance())
+    # print("\n--- Getting Account Balance ---")
+    # print(broker.get_balance())
 
-    print("\n--- Checking Pending Orders ---")
-    print(broker.get_pending_count())
+    # print("\n--- Checking Pending Orders ---")
+    # print(broker.get_pending_count())
 
-    # Uncomment these to test trading actions:
-    # print(place_order("BTC", "BUY", 0.01, price=95000))  # LIMIT
-    print(broker.place_order("BNB/USD", "BUY", 1))      
-    print(broker.place_order("BNB/USD", "SELL", 1))             # MARKET       
-    print(broker.query_order(pair="BNB/USD", pending_only=False))
-    # print(cancel_order(pair="BNB/USD"))
+    # # Uncomment these to test trading actions:
+    # # print(place_order("BTC", "BUY", 0.01, price=95000))  # LIMIT
+    # print(broker.place_order("BNB/USD", "BUY", 1))      
+    # print(broker.place_order("BNB/USD", "SELL", 1))             # MARKET       
+    # print(broker.query_order(pair="BNB/USD", pending_only=False))
+    # # print(cancel_order(pair="BNB/USD"))
